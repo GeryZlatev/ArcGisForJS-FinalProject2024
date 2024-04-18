9d0b11cdcf4b8968108396fd8672de7d30f35a13
GeryZlatev <gery.zlatev@gmail.com>
2024-04-18 11:20:54 +0300

    add home button

--- map.js
+++ map.js
@@ -1,16 +1,22 @@
-require(["esri/config", "esri/WebMap", "esri/views/MapView"],
-    function (esriConfig, WebMap, MapView) {
+require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home",],
+    function (esriConfig, WebMap, MapView, Home) {
 
         esriConfig.apiKey = "AAPK77ebc8a134de4074a160da0e32d0d877onv_cK6xV7_3fD5hbgt8oYIPGcwHVf3SZVZkhweY7eONVEXqwdhRAkgphMmXCg9x";
 
         const webmap = new WebMap({
             portalItem: {
                 id: "232b4d297d054b2a831a3ce629ac8495"
-            }
+            },
         });
 
         const view = new MapView({
             container: "viewDiv",
-            map: webmap
-        })
+            map: webmap,
+        });
+
+        const homeBtn = new Home({
+            view,
+        });
+        
+        view.ui.add(homeBtn, "bottom-left");
     })
