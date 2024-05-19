188828190848702a5cc9c1381cb49f5e0ea1c0f3
GeryZlatev <gery.zlatev@gmail.com>
2024-05-19 21:07:21 +0300

    add basemap gallery

--- map.js
+++ map.js
@@ -3,9 +3,17 @@ require([
     "esri/WebMap",
     "esri/views/MapView",
     "esri/widgets/Home",
-    "esri/widgets/LayerList"
+    "esri/widgets/LayerList",
+    "esri/widgets/BasemapGallery",
 ],
-    function (esriConfig, WebMap, MapView, Home, LayerList) {
+    function (
+        esriConfig,
+        WebMap,
+        MapView,
+        Home,
+        LayerList,
+        BasemapGallery,
+    ) {
 
         esriConfig.apiKey = "AAPK77ebc8a134de4074a160da0e32d0d877onv_cK6xV7_3fD5hbgt8oYIPGcwHVf3SZVZkhweY7eONVEXqwdhRAkgphMmXCg9x";
 
@@ -30,14 +38,35 @@ require([
             view,
         });
 
-        view.ui.add("layer-list-btn","top-right");
+        view.ui.add("layer-list-btn", "top-right");
         view.ui.add(layerList, "top-right");
+        view.ui.add("basemap-gallery-btn", "top-right")
 
-        document.getElementById("layer-list-btn").addEventListener("click", function() {
-            const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
-            const currentProp = layerListEl.style.getPropertyValue("display");
+        const basemapGalery = new BasemapGallery({
+            view
+        });
 
-            layerListEl.style.setProperty("display", currentProp == "none" ? "block" : "none");
-        })
-        
-    })
+        view.ui.add(basemapGalery, "top-right");
+
+        document.getElementById("layer-list-btn").addEventListener("click", function () {
+            toggleButton("layerList");
+        });
+
+        document.getElementById("basemap-gallery-btn").addEventListener("click", function(){
+            toggleButton("gallery");
+        });
+
+        function toggleButton(el) {
+            if(el == "layerList") {
+                const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+                const currentProp = layerListEl.style.getPropertyValue("display");
+                layerListEl.style.setProperty("display", currentProp == "none" ? "block" : "none");
+            } else if ("gallery") {
+                const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
+                const currentPropGallery = galleryEl.style.getPropertyValue("display");
+                galleryEl.style.setProperty("display", currentPropGallery == "none" ? "block" : "none");
+            }
+
+        }
+
+    });
